1f1676a1728f477af2fa3eca771c1df80edb5414
Saadat Ali <saadatlucky313@gmail.com>
2022-05-28 01:14:32 +0500

    Create Test For FizzBuzz Function

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -21,4 +21,19 @@ describe Solver do
       expect(@solver.reverse(string)).to eql 'DCBA'
     end
   end
+
+  describe 'FizzBuzz Function' do
+    it 'Returns "fizz" when number is divisible by 3' do
+      expect(@solver.fizzbuzz(6)).to eq 'fizz'
+    end
+    it 'Returns "buzz" when number is divisible by 5' do
+      expect(@solver.fizzbuzz(10)).to eq 'buzz'
+    end
+    it 'Returns "fizzbuzz" when number is divisible by 3 and 15' do
+      expect(@solver.fizzbuzz(15)).to eq 'fizzbuzz'
+    end
+    it 'Returns number as a string if it is not divisible by 5 or 3' do
+      expect(@solver.fizzbuzz(8)).to eq '8'
+    end
+  end
 end
